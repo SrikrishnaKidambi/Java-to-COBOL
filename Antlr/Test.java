404201ad527493d18069df295cfec07b1d00dce8
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2025-06-17 13:01:56 +0530

    no chnages

--- Antlr/Test.java
+++ Antlr/Test.java
@@ -1,67 +1,21 @@
+//done ✅
 import java.util.Scanner;
 
-
-
 public class Test {
-
-
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter day number (1-7): ");
+        int day = sc.nextInt();
 
-        for(int i=0;i<9;i++){
-
-            int j=i;
-
-            while(j>0){
-
-                if(i==0){
-
-                    System.out.println("Hello");
-
-                    if(j==0){
-
-                        System.out.println("WOW");
-
-                    }else if(i==1){
-
-                        System.out.println("Nice");
-
-                        if(i%2==0){
-
-                            System.out.println("I am batman");
-
-                        }
-
-                    }
-
-                }
-
-                switch (j) {
-
-                    case 1:
-
-                        System.out.println("this is 1");
-
-                        break;
-
-                    case 2: 
-
-                    System.out.println("This is 2");
-
-                    break;
-
-                    default:
-
-                        break;
-
-                }
-
-            }
-
-
-
+        switch (day) {
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday"); break;
+            case 7: System.out.println("Sunday"); break;
+            default: System.out.println("Invalid day number");
         }
-
     }
-
 }
