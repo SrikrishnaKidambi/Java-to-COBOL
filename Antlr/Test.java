7de3019eb67229e9c2b2b5f0115e8f210a615f05
krishnabrahmajosyula <krishnabrahmajosyula@gmail.com>
2025-06-17 13:05:35 +0530

    integration to new java code generation

--- Antlr/Test.java
+++ Antlr/Test.java
@@ -1,67 +1,6 @@
-import java.util.Scanner;
-
-
-
 public class Test {
 
-
-
     public static void main(String[] args) {
-
-        for(int i=0;i<9;i++){
-
-            int j=i;
-
-            while(j>0){
-
-                if(i==0){
-
-                    System.out.println("Hello");
-
-                    if(j==0){
-
-                        System.out.println("WOW");
-
-                    }else if(i==1){
-
-                        System.out.println("Nice");
-
-                        if(i%2==0){
-
-                            System.out.println("I am batman");
-
-                        }
-
-                    }
-
-                }
-
-                switch (j) {
-
-                    case 1:
-
-                        System.out.println("this is 1");
-
-                        break;
-
-                    case 2: 
-
-                    System.out.println("This is 2");
-
-                    break;
-
-                    default:
-
-                        break;
-
-                }
-
-            }
-
-
-
-        }
-
+        System.err.println("HI");
     }
-
 }
--- Antlr/cobol_test1.cbl
+++ Antlr/cobol_test1.cbl
@@ -7,7 +7,3 @@
       * Variables for Scope:METHOD_MAIN_CLASS_TEST_GLOBAL
        01  ARGS_MAIN_-ARRAY.
            05  args_main    PIC X(100) OCCURS 100 TIMES.
-      * Variables for Scope:FOR_BLOCK_METHOD_MAIN_CLASS_TEST_GLOBAL
-       01  i_main          PIC S9(5).
-      * Variables for Scope:BLOCK_FOR_BLOCK_METHOD_MAIN_CLASS_TEST_GLOBAL
-       01  j_main          PIC S9(5).
