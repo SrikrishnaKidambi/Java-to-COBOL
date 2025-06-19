737b0413d1bca9c268117f554a5f7f47c07138f2
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2025-06-19 15:57:07 +0530

    minor changes

--- Antlr/TestScoped.java
+++ Antlr/TestScoped.java
@@ -1,6 +1,24 @@
+//done ✅
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args_main) {
-        System.err.println("HI");
+        Scanner sc_main = new Scanner(System.in);
+        System.out.print("Find primes up to: ");
+        int n_main = sc_main.nextInt();
+        for (int i_main = 2; i_main <= n_main; i_main++) {
+            boolean isPrime_main = true;
+            for (int j_main = 2; j_main <= i_main / 2; j_main++) {
+                int rem_main = i_main % j_main;
+                if (rem_main == 0) {
+                    isPrime_main = false;
+                    break;
+                }
+            }
+            if (isPrime_main) {
+                System.out.println(i_main + " is prime");
+            }
+        }
     }
 }
--- Antlr/main.cbl
+++ Antlr/main.cbl
@@ -5,5 +5,9 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
       * Variables for Method:main
+           05  sc_main      PIC X(100).
+           05  n_main       PIC S9(5).
            05  i_main       PIC S9(5).
+           05  isPrime_main PIC X(1).
            05  j_main       PIC S9(5).
+           05  rem_main     PIC S9(5).
