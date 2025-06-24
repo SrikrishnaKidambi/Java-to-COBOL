ece642ac59eb1d8d8d6dc062426e47f0c3f206ae
krishnabrahmajosyula <krishnabrahmajosyula@gmail.com>
2025-06-24 11:46:38 +0530

    minor bug fix in handling method translation

--- Antlr/Test.java
+++ Antlr/Test.java
@@ -1,24 +1,12 @@
-//done ✅
-import java.util.Scanner;
-
 public class Test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Find primes up to: ");
-        int n = sc.nextInt();
 
-        for (int i = 2; i <= n; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j <= i / 2; j++) {
-                int rem = i % j;
-                if (rem == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) {
-                System.out.println(i + " is prime");
-            }
-        }
+    private static void add(int x,int y){
+        int z=x+y;
+        System.out.println("The addition is :"+z);
+    }
+    public static void main(String[] args) {
+        int x=0;
+        int u=1;
+        add(x, u);
     }
 }
--- Antlr/TestScoped.java
+++ Antlr/TestScoped.java
@@ -1,24 +1,13 @@
-//done ✅
-import java.util.Scanner;
-
 public class Test {
 
+    private static void add(int x_add, int y_add) {
+        int z_add = x_add + y_add;
+        System.out.println("The addition is :" + z_add);
+    }
+
     public static void main(String[] args_main) {
-        Scanner sc_main = new Scanner(System.in);
-        System.out.print("Find primes up to: ");
-        int n_main = sc_main.nextInt();
-        for (int i_main = 2; i_main <= n_main; i_main++) {
-            boolean isPrime_main = true;
-            for (int j_main = 2; j_main <= i_main / 2; j_main++) {
-                int rem_main = i_main % j_main;
-                if (rem_main == 0) {
-                    isPrime_main = false;
-                    break;
-                }
-            }
-            if (isPrime_main) {
-                System.out.println(i_main + " is prime");
-            }
-        }
+        int x_main = 0;
+        int u_main = 1;
+        add(x_main, u_main);
     }
 }
