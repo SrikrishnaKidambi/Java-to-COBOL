1c05af3524633e3c69a961c0fddd410889a59eca
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2025-07-22 15:24:16 +0530

    minor changes

--- finalCobolCode.cbl
+++ finalCobolCode.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. DigitCounter.
+       PROGRAM-ID. DayOfWeek.
 
 
        DATA DIVISION.
@@ -9,20 +9,31 @@
            05  args_main    PIC X(100) OCCURS 100 TIMES.
       * Variables for Scope:BLOCK_METHOD_MAIN_CLASS_TESTSCOPED_GLOBAL
        01  sc_main         PIC X(100).
-       01  num_main        PIC S9(5).
-       01  count_main      PIC S9(5).
+       01  day_main        PIC S9(5).
 
 
 
        MAIN-PARA.
       * Input from ODT is enabled
-       DISPLAY "Enter number: " WITH NO ADVANCING
-       ACCEPT num_main
-       MOVE 0 TO count_main
-       PERFORM UNTIL NOT (num_main NOT= 0)
-       ADD 1 TO count_main
-       DIVIDE num_main BY 10 GIVING num_main
-       END-PERFORM
-       DISPLAY "Number of digits: "   count_main
+       DISPLAY "Enter day number (1-7): " WITH NO ADVANCING
+       ACCEPT day_main
+       EVALUATE day_main
+       WHEN 1
+       DISPLAY "Monday"
+       WHEN 2
+       DISPLAY "Tuesday"
+       WHEN 3
+       DISPLAY "Wednesday"
+       WHEN 4
+       DISPLAY "Thursday"
+       WHEN 5
+       DISPLAY "Friday"
+       WHEN 6
+       DISPLAY "Saturday"
+       WHEN 7
+       DISPLAY "Sunday"
+       WHEN OTHER
+       DISPLAY "Invalid day number"
+       END-EVALUATE
        STOP RUN.
 
