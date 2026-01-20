ec0c763d1c85addfb2564b95ea380630f9b46056
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2026-01-20 17:49:17 +0530

    extracted x cobol

--- Dataset Pipeline/fileCount.py
+++ Dataset Pipeline/fileCount.py
@@ -1,7 +1,8 @@
 import os
 
 folder = "./COBOL_files"  # folder you want to check
-folder1 = "./Java_files"
+# folder1 = "./Java_files"
+folder1 = "./X_COBOL_files"
 num_files = len([f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))])
 num_files1 = len([f for f in os.listdir(folder1) if os.path.isfile(os.path.join(folder1, f))])
 print(f"Number of files in {folder}: {num_files}")
